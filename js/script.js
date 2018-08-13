c0e02f5dc455797f8398a5b46a96eb001a4dbefa
Muhammad Aliyul Murtadlo <muhammadaliyulm@gmail.com>
2018-08-13 14:44:08 +0700

    section kegiatan dan galeri

--- js/script.js
+++ js/script.js
@@ -72,3 +72,31 @@ $('a[href*="#"]')
     }
 });
 
+// Instafeed
+$(document).ready(function() {
+
+
+    var userFeed = new Instafeed({
+        get: 'user',
+        userId: '6240737317',
+        resolution: 'standard_resolution',
+        accessToken: '6240737317.1677ed0.0c639ef3f8024dd4b5407b2412c40049',
+        sortBy: 'most-recent',
+        template: '<div class="col-lg-3 instaimg"><a href="{{image}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a></div>',
+    });
+
+
+    userFeed.run();
+
+    
+    // This will create a single gallery from all elements that have class "gallery-item"
+    $('.gallery').magnificPopup({
+        type: 'image',
+        delegate: 'a',
+        gallery: {
+            enabled: true
+        }
+    });
+
+
+});
